bb3846b066a26f3312ac197ba71120947859de87
BarnabyRumbold <barnabyrumbold@hotmail.com>
2022-11-28 16:41:58 +0100

    commit

--- packages/joke/joke/__main__.py
+++ packages/joke/joke/__main__.py
@@ -1,20 +1,20 @@
-#import pyjokes
+import pyjokes
 # import pandas as pd
-from fetch_data import get_data
-
-# def main(args):
-#   joke = pyjokes.get_joke()
-#   return {
-#     'body': {
-#       'response_type': 'in_channel',
-#       'text': joke
-#     }
-#   }
+# from fetch_data import get_data
 
 def main(args):
-    return {
-      'body': {
-         'text': get_data
-      }
+  joke = pyjokes.get_joke()
+  return {
+    'body': {
+      'response_type': 'in_channel',
+      'text': joke
     }
+  }
+
+# def main(args):
+#     return {
+#       'body': {
+#          'text': "hey"
+#       }
+#     }
 
